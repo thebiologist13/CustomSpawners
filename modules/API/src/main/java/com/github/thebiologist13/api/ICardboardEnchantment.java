6b41fc2146fa52310aaa0d62dbf93e881fa61dcb
thebiologist13 <thebiologist13@gmail.com>
2013-04-09 16:24:17 -0600

    Remove duplicates?

--- modules/API/src/main/java/com/github/thebiologist13/api/ICardboardEnchantment.java
+++
@@ -1,9 +0,0 @@
-package com.github.thebiologist13.api;
-
-import org.bukkit.enchantments.Enchantment;
-
-public interface ICardboardEnchantment {
-	
-	public Enchantment unbox();
-	
-}
--- modules/API/src/main/java/com/github/thebiologist13/api/IConverter.java
+++
@@ -1,7 +0,0 @@
-package com.github.thebiologist13.api;
-
-public interface IConverter {
-	
-	public void convert(ISpawner spawner);
-	
-}
